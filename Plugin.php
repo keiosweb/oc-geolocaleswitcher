d042925fd0210900a244c7f8ebe6cf7d4d703626
Lukasz <lukasz@c-call.eu>
2014-12-08 21:50:42 +0100

    initial commit

---
+++ Plugin.php
@@ -0,0 +1,70 @@
+<?php namespace Voipdeploy\GeoLocaleSwitcher;
+
+use RainLab\Translate\Models\Locale as LocaleModel;
+use RainLab\Translate\Classes\Translator;
+use System\Classes\PluginBase;
+use Redirect;
+use Request;
+use App;
+use Config;
+use Session;
+
+
+/**
+ * GeoLocaleSwitcher Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'GeoLocaleSwitcher',
+            'description' => 'Provides language switching with MaxMind GeoIP Database',
+            'author' => 'Voipdeploy',
+            'icon' => 'icon-leaf'
+        ];
+    }
+
+    public function registerComponents()
+    {
+        return [
+            'Voipdeploy\GeoLocaleSwitcher\Components\GeoLocaleSwitcher' => 'geoLocaleSwitcher'
+        ];
+    }
+
+    public function boot()
+    {
+        $firstSegment = Request::segment(1);
+        $availableLocales = LocaleModel::listAvailable();
+
+        App::register('Voipdeploy\GeoIP\GeoIPServiceProvider');
+
+        Config::set('geoip::service', 'maxmind');
+        Config::set('geoip::maxmind.type', 'database');
+        Config::set('geoip::maxmind.database_path', base_path() . '/plugins/voipdeploy/geolocaleswitcher/database/database.mmdb');
+
+        if (Session::has(Translator::SESSION_LOCALE)) {
+            return;
+        }
+
+        $geoIP = App::make('geoip');
+
+        $location = $geoIP->getLocation();
+
+        $isoCodeFromIP = strtolower($location['isoCode']);
+
+        $backendUri = Config::get('cms.backendUri');
+
+        if (array_key_exists($isoCodeFromIP, $availableLocales) && (strpos(Request::path(), $backendUri) === false) && (!array_key_exists($firstSegment, $availableLocales))) {
+            Redirect::to($isoCodeFromIP . Request::getRequestUri())->withInput()->send();
+            App::shutdown();
+            exit();
+        }
+    }
+
+}
---
+++ components/GeoLocaleSwitcher.php
@@ -0,0 +1,56 @@
+<?php namespace Voipdeploy\Geolocaleswitcher\Components;
+
+use RainLab\Translate\Classes\Translator;
+use RainLab\Translate\Models\Locale;
+use Cms\Classes\ComponentBase;
+use Cms\Classes\Page;
+use App;
+
+class GeoLocaleSwitcher extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name' => 'voipdeploy.geolocaleswitcher::lang.component.title', //GeoLocaleSwitcher
+            'description' => 'voipdeploy.geolocaleswitcher::lang.component.description' //Displays locale switcher with current geoIP country
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [
+            'localeChangePage' => [
+                'title' => 'voipdeploy.geolocaleswitcher::lang.component.localeChangePage.title',
+                'description' => 'voipdeploy.geolocaleswitcher::lang.component.localeChangePage.description',
+                'type' => 'dropdown'
+            ]
+        ];
+    }
+
+    public function getLocaleChangePageOptions()
+    {
+        return ['' => '- none -'] + Page::sortBy('baseFileName')->lists('baseFileName', 'url');
+    }
+
+    public function localeChangePage()
+    {
+        $localeChangePage = $this->property('localeChangePage');
+        return ($localeChangePage ?: null);
+    }
+
+    public function detectedCountry()
+    {
+        $geoIP = App::make('geoip');
+        $location = $geoIP->getLocation();
+        return $location['country'];
+    }
+
+    public function currentLocale()
+    {
+        $currentLocale = Translator::instance()->getLocale();
+        $allAvailableLocale = Locale::listAvailable();
+        return $allAvailableLocale[$currentLocale];
+    }
+
+}
