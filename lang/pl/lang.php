5851edf1e6932e76f38e2a303122843830af8546
Kuba <jakub@c-call.eu>
2015-01-07 15:04:44 +0100

    locale

---
+++ lang/pl/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+'plugin' => [
+'name' => 'GeoLocaleSwitcher',
+'description' => 'Zmień język w zależności od lokacji odwiedzającego'
+],
+
+
+'component' => [
+'title' => 'Kraj i Język',
+'description' => 'Wyświetla kraj odwiedzjającego i aktywny język'
+
+],
+
+'localeChangePage' => [
+'title' => 'Zmień język',
+'description' => 'Strona zmiany języka'
+
+],
+
+];
