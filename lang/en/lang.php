f62742e8544eb4a14e0b11df91f6c72f70e57d8c
Kuba <jakub@c-call.eu>
2015-01-07 15:02:52 +0100

    locale

---
+++ lang/en/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+'plugin' => [
+'name' => 'GeoLocaleSwitcher',
+'description' => 'Switch language depending on visitor location'
+],
+
+
+'component' => [
+'title' => 'Show GeoLocale',
+'description' => 'Show visitor\'s country and current locale'
+
+],
+
+'localeChangePage' => [
+'title' => 'Change Locale',
+'description' => 'Switch language page'
+
+],
+
+];
